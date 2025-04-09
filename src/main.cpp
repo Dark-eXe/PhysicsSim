bbe7dda25196504ae915db8b4e53429aebeca2f8
Dark-eXe <andrewtur510@gmail.com>
2025-04-09 01:38:22 -0700

    Draw circle

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "gravity.h"
+#include "constants.h"
 
 int main()
 {
+    // Create GLFW window
     GLFWwindow *window = StartGLFW();
     if (!window)
     {
@@ -16,10 +18,38 @@ int main()
         glfwTerminate();
         return -1;
     }
+    glfwMakeContextCurrent(window);
+    glewInit();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, Constants::WINDOW_WIDTH, 0.0, Constants::WINDOW_HEIGHT, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f); // dark gray background
 
+    // Circle
+    float centerX = Constants::WINDOW_WIDTH / 2.0f;
+    float centerY = Constants::WINDOW_HEIGHT / 2.0f;
+    float radius = 50.0f;
+    int res = 100;
+
+    // Run GLFW window
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Circle
+        glBegin(GL_TRIANGLE_FAN);
+        glVertex2d(centerX, centerY);
+        for (int i = 0; i <= res; ++i)
+        {
+            float angle = 2.0f * Constants::PI * (static_cast<float>(i) / res);
+            float x = centerX + cos(angle) * radius;
+            float y = centerY + sin(angle) * radius;
+            glVertex2d(x, y);
+        }
+        glEnd();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
